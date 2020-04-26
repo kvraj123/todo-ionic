87604e111e6f8f7ae5219ffdb7ce2bd3990351db
kvraj123 <poojarykaviraj7@gmail.com>
2020-04-26 20:32:11 +0530

    int commit

---
+++ src/app/auth/auth-response.ts
@@ -0,0 +1,13 @@
+export interface AuthResponse {
+    token: string,
+    user: {
+    age: number
+    createdAt: Date
+    email: string
+    name: string
+    updatedAt: Date
+    __v: number
+    _id: string
+    }
+    
+}
